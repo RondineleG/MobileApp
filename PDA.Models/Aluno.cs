a8b3ed086766ae0aa746a79852acd8dd704b3ea7
Rondinele <rondineleg@edu.univali.br>
2019-10-31 13:37:17 -0300

    organizando modelo

--- PDA.Models/Aluno.cs
+++ PDA.Models/Aluno.cs
@@ -3,8 +3,7 @@
 
     public class Aluno
     {        
-        public int id { get; set; }
-
+           public int id { get; set; }
         public string nome { get; set; }
         public string sobrenome { get; set; }
         public string telefone { get; set; }
